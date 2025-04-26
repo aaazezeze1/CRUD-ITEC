32bb105ca325fd1e739954b1b596ae0375a78ff4
CryptedTNT <167340107+CryptedTNT@users.noreply.github.com>
2025-04-26 23:22:56 +0800

    new

---
+++ CRUD-ITEC-main/database.php
@@ -0,0 +1,18 @@
+<?php  
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "db_pms";
+
+// connect to mysql server
+try 
+{
+    $conn = mysqli_connect($servername, $username, $password, $db_name);
+    // echo "You are connected!<br>";
+}
+catch(mysqli_sql_exception) {
+    echo "Could not connect!<br>";
+}
+
+?>
